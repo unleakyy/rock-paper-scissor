fc536ee03b2fdd06c6c454b4c7d4a808d32da5ff
Kyle Yuan <yuan.superhot@gmail.com>
2024-08-22 17:39:09 +0800

    alternative method

--- scripts/shared.js
+++
@@ -1,8 +0,0 @@
-export function setUserPick(value) {
-  localStorage.setItem("userPick", value);
-}
-
-export function getUserPick() {
-  let userPick = localStorage.getItem("userPick");
-  return userPick;
-}
